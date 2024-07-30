1ed74ee9a3fc0c6d61879a6d8109d562697606b0
Srikasun <sri07072005@gmail.com>
2024-07-30 10:26:46 +0530

    commit

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,66 +2,82 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = 'http://192.168.47.180:5555';
+  static const String baseUrl = 'http://192.168.106.180';
 
   static Future<http.Response> loginUser(String email, String password) async {
-    final response = await http.post(
-      Uri.parse('$baseUrl/users/login'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
-        'email': email,
-        'password': password,
-      }),
-    );
-
-    return response;
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/api/users/login'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'email': email,
+          'password': password,
+        }),
+      );
+      return response;
+    } catch (e) {
+      print('Error logging in: $e');
+      rethrow;
+    }
   }
 
   static Future<http.Response> registerUser(
       String username, String email, String password) async {
-    final response = await http.post(
-      Uri.parse('$baseUrl/users/add-user'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
-        'username': username,
-        'email': email,
-        'password': password,
-      }),
-    );
-
-    return response;
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/api/users/add-user'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'username': username,
+          'email': email,
+          'password': password,
+        }),
+      );
+      return response;
+    } catch (e) {
+      print('Error registering user: $e');
+      rethrow;
+    }
   }
 
   static Future<http.Response> createPost(
       String title, String content, String category, String username) async {
-    final response = await http.post(
-      Uri.parse('$baseUrl/posts'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
-        'title': title,
-        'content': content,
-        'category': category,
-        'username': username,
-      }),
-    );
-
-    return response;
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/api/posts'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'title': title,
+          'content': content,
+          'category': category,
+          'username': username,
+        }),
+      );
+      return response;
+    } catch (e) {
+      print('Error creating post: $e');
+      rethrow;
+    }
   }
 
   static Future<http.Response> fetchPosts() async {
-    final response = await http.get(
-      Uri.parse('$baseUrl/posts'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-    );
-
-    return response;
+    try {
+      final response = await http.get(
+        Uri.parse('$baseUrl/api/posts'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+      return response;
+    } catch (e) {
+      print('Error fetching posts: $e');
+      rethrow;
+    }
   }
 }
